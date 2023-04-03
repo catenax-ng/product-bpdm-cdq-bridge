5ad6aee67ecf7656889c65536435be683b2ecc55
fabio.d.mota <fabio.d.mota@cgi.com>
2023-04-03 14:28:39 +0100

    feat(cdq-bridge): Add oAuth2 for bridge endpoints

--- src/main/kotlin/com/catenax/bpdm/bridge/cdq/controller/SaasController.kt
+++ src/main/kotlin/com/catenax/bpdm/bridge/cdq/controller/SaasController.kt
@@ -19,14 +19,51 @@
 
 package com.catenax.bpdm.bridge.cdq.controller
 
+
+import io.swagger.v3.oas.annotations.Operation
+import io.swagger.v3.oas.annotations.responses.ApiResponse
+import io.swagger.v3.oas.annotations.responses.ApiResponses
+import org.eclipse.tractusx.bpdm.common.dto.request.PaginationRequest
+import org.eclipse.tractusx.bpdm.common.dto.response.PageResponse
+import org.eclipse.tractusx.bpdm.pool.api.model.ImportIdEntry
+import org.springdoc.core.annotations.ParameterObject
+import org.springframework.core.env.Environment
 import org.springframework.web.bind.annotation.*
 
 @RestController
 @RequestMapping("/api/saas")
-class SaasController {
+class SaasController(
+    val environment: Environment
+) {
 
-    @GetMapping
-    fun dummyEndPoint(): String {
+
+    @Operation(
+        summary = "Paginate Identifier Mappings by CX-Pool Identifiers",
+        description = "Paginate through all CX-Pool Identifier and Business Partner Number mappings."
+    )
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "200", description = "The found import identifier mappings"),
+            ApiResponse(
+                responseCode = "400",
+                description = "On malformed requests or exceeding the request size of "
+            ),
+        ]
+    )
+    @GetMapping("/identifier-mappings")
+    fun getImportEntries(@ParameterObject paginationRequest: PaginationRequest): PageResponse<ImportIdEntry> {
+        return PageResponse(0, 1, 1, 1, emptyList())
+    }
+
+    @Operation(summary = "Throw a not implemented error")
+    @ApiResponses(
+        value = [
+            ApiResponse(responseCode = "500", description = "Not implemented error")
+        ]
+    )
+    @GetMapping("/endpoint")
+    fun throwNotImplementedError(): String {
         throw NotImplementedError("This endpoint is not implemented yet.")
     }
+
 }
