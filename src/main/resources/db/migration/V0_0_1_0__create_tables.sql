fb8beda0f719ecb9306863df7f783e3d01ec1648
fabio.d.mota <fabio.d.mota@cgi.com>
2023-04-12 18:21:34 +0100

    fix(cdq-bridge): add PK to table

--- src/main/resources/db/migration/V0_0_1_0__create_tables.sql
+++ src/main/resources/db/migration/V0_0_1_0__create_tables.sql
@@ -7,5 +7,6 @@ CREATE TABLE import_entries
     created_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     updated_at TIMESTAMP WITHOUT TIME ZONE NOT NULL,
     bpn        VARCHAR(255)                NOT NULL,
-    import_id  varchar(255)                NOT NULL
+    import_id  varchar(255)                NOT NULL,
+    PRIMARY KEY (id)
 );
